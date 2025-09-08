2b30dfc17aecdbadb2b37ea497ad36fcc0342c91
my-digital-life <152349349+my-digital-life@users.noreply.github.com>
2025-09-08 02:06:31 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,28 +4,30 @@
 
 # -------------------------------
 # Update package lists
+sudo apt update
 # -------------------------------
-run_cmd "sudo apt update"
 
 # -------------------------------
 # Upgrade all packages to latest versions
 # -------------------------------
-run_cmd "sudo apt -y upgrade"
+sudo apt -y upgrade
 
 # -------------------------------
 # Install SSH and OpenSSH server
 # -------------------------------
-run_cmd "sudo apt -y install ssh openssh-server open-vm-tools samba cifs-utils smbclient"
+sudo apt -y install ssh openssh-server open-vm-tools samba cifs-utils smbclient
 
 # -------------------------------
 # Enable root login via SSH
 # This modifies the sshd_config file to allow root login
 # -------------------------------
-run_cmd "sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 # -------------------------------
 # Restart SSH service to apply changes
 # -------------------------------
-run_cmd "sudo systemctl restart ssh"
+sudo systemctl restart ssh
 
+# -------------------------------
 echo "[INFO] Setup complete. Run ./setup.sh"
+# -------------------------------
